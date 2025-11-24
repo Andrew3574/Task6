bdcc4c94014620bf37f3e98c3942aec4cb944f5c
Burlakov Andrey <anderson64217@gmail.com>
2025-11-24 12:25:04 +0300

    test2 deleted

--- EditorAPI/Repositories/BaseRepository.cs
+++ EditorAPI/Repositories/BaseRepository.cs
@@ -18,7 +18,6 @@ namespace Repositories
         public abstract Task Create(T entity);
         public abstract Task Update(T entity);
         public abstract Task Delete(T entity);
-        protected void Send(){System.Console.WriteLine("test2");}
         protected void Send(){System.Console.WriteLine("feature test");}
     }
 }
